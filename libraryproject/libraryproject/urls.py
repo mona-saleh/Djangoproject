985f87f89e07d088a0865215f006a65a158350a4
mona-saleh <mona.alzunidi17@gmail.com>
2025-10-05 23:22:02 +0300

    Added user module and fixed book module templates

---
+++ libraryproject/libraryproject/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("books/", include("apps.bookmodule.urls")),
+    path("users/", include("apps.usermodule.urls")),
+]
